59d3816bef8b0aa9d1aafb075006d95876fea578
Siddu Navaneetha Raj <sn5@illinois.edu>
2018-09-19 16:54:00 -0500

    First commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println("Ran");
+        String url = "http://erdani.com/tdpl/hamlet.txt";
+        String n = URLtoString.urlToString("http://erdani.com/tdpl/hamlet.txt");
+        System.out.println(n);
     }
 }
